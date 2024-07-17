76118bf3612bb423583e4332814cdeacc00e8dfb
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-07-18 00:25:37 +0300

    All expenses header

---
+++ lib/widgets/range_options.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_admin_dashboard/core/utils/app_styles.dart';
+
+class RangeOptions extends StatelessWidget {
+  const RangeOptions({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(width: 1, color: Color(0xffF1F1F1)),
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: const Row(
+        children: [
+          Text(
+            "Monthly",
+            style: AppStyles.styleMedium16,
+          ),
+          SizedBox(width: 18),
+          Icon(
+            Icons.keyboard_arrow_down_rounded,
+            color: Color(0xFF064061),
+          ),
+        ],
+      ),
+    );
+  }
+}
