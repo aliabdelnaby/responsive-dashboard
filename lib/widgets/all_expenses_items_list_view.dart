776aa8e7eb4c880246ae4875ab701aa6eef11c30
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-07-18 20:41:52 +0300

    All expenses section

--- lib/widgets/all_expenses_items_list_view.dart
+++ lib/widgets/all_expenses_items_list_view.dart
@@ -3,55 +3,78 @@ import 'package:responsive_admin_dashboard/core/models/all_expenses_item_model.d
 import 'package:responsive_admin_dashboard/core/utils/app_images.dart';
 import 'package:responsive_admin_dashboard/widgets/all_expenses_item.dart';
 
-class AllExpensesItemsListView extends StatelessWidget {
+class AllExpensesItemsListView extends StatefulWidget {
   const AllExpensesItemsListView({super.key});
-  static const items = [
-    AllExpensesItemModel(
+
+  @override
+  State<AllExpensesItemsListView> createState() =>
+      _AllExpensesItemsListViewState();
+}
+
+class _AllExpensesItemsListViewState extends State<AllExpensesItemsListView> {
+  final items = [
+    const AllExpensesItemModel(
       image: Assets.imagesBalance,
       title: "Blance",
       date: "April 2022",
       price: r"$20,129",
     ),
-    AllExpensesItemModel(
+    const AllExpensesItemModel(
       image: Assets.imagesIncome,
       title: "Income",
       date: "April 2022",
       price: r"$20,129",
     ),
-    AllExpensesItemModel(
+    const AllExpensesItemModel(
       image: Assets.imagesExpenses,
       title: "Expenses",
       date: "April 2022",
       price: r"$20,129",
     ),
   ];
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Row(
-      children:
-          // items.map((e) => AllExpensesItem(allExpensesItemModel: e)).toList(),
-          items.asMap().entries.map((e) {
+      children: items.asMap().entries.map((e) {
         int index = e.key;
         var item = e.value;
         if (index == 1) {
           return Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: AllExpensesItem(
-                allExpensesItemModel: item,
-                isSelected: false,
+            child: GestureDetector(
+              onTap: () {
+                updateIndex(index);
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: AllExpensesItem(
+                  allExpensesItemModel: item,
+                  isSelected: selectedIndex == index,
+                ),
               ),
             ),
           );
         } else {
           return Expanded(
-            child: AllExpensesItem(
-              allExpensesItemModel: item,
-              isSelected: false,
+            child: GestureDetector(
+              onTap: () {
+                updateIndex(index);
+              },
+              child: AllExpensesItem(
+                allExpensesItemModel: item,
+                isSelected: selectedIndex == index,
+              ),
             ),
           );
         }
       }).toList(),
     );
+  }
+
+  void updateIndex(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
   }
 }
