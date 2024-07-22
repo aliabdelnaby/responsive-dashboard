14e64b44e71612e1cf9ac5d53f2e0038583a6727
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-07-22 15:38:12 +0300

    My card done

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:responsive_admin_dashboard/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_admin_dashboard/widgets/custom_drawer.dart';
+import 'package:responsive_admin_dashboard/widgets/my_card.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
   const DashBoardDesktopLayout({super.key});
@@ -17,6 +18,10 @@ class DashBoardDesktopLayout extends StatelessWidget {
         Expanded(
           flex: 2,
           child: AllExpensesAndQuickInvoiceSection(),
+        ),
+        SizedBox(width: 24),
+        Expanded(
+          child: MyCard(),
         ),
       ],
     );
---
+++ lib/widgets/my_card.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:responsive_admin_dashboard/core/utils/app_images.dart';
+import 'package:responsive_admin_dashboard/core/utils/app_styles.dart';
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 420 / 215,
+      child: Container(
+        decoration: ShapeDecoration(
+          image: const DecorationImage(
+            image: AssetImage(
+              Assets.imagesCardBackground,
+            ),
+          ),
+          color: const Color(0xff4EB7F2),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding:
+                  const EdgeInsets.only(right: 42, left: 32, top: 16),
+              title: Text(
+                "Name Card",
+                style: AppStyles.styleRegular16.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: const Text(
+                "Ali Abdelnaby",
+                style: AppStyles.styleMedium20,
+              ),
+              trailing: SvgPicture.asset(
+                Assets.imagesGallery,
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    "5533 3333 3333 3335",
+                    style:
+                        AppStyles.styleSemiBold24.copyWith(color: Colors.white),
+                  ),
+                  Text(
+                    "12/25 - 124",
+                    style:
+                        AppStyles.styleRegular16.copyWith(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 26),
+          ],
+        ),
+      ),
+    );
+  }
+}
