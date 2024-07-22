f7715a34dbd1bd12f9da1e95312ef8250cb13d34
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-07-22 16:34:59 +0300

    Dots Indicator

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:responsive_admin_dashboard/views/dots_indicator.dart';
 import 'package:responsive_admin_dashboard/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_admin_dashboard/widgets/custom_drawer.dart';
 import 'package:responsive_admin_dashboard/widgets/my_cards_page_view.dart';
@@ -20,9 +21,8 @@ class DashBoardDesktopLayout extends StatelessWidget {
           child: AllExpensesAndQuickInvoiceSection(),
         ),
         SizedBox(width: 24),
-        Expanded(
-          child: MyCardsPageView(),
-        ),
+        // Expanded(child: MyCardsPageView()),
+        DotsIndicator(),
       ],
     );
   }
