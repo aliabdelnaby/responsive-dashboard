ad64d26e3ffee9496d8455bacb3d65e499ae15e0
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-07-23 21:10:15 +0300

    Responsive desktop layout [1] [2]

--- lib/widgets/income_details.dart
+++ lib/widgets/income_details.dart
@@ -28,12 +28,8 @@ class IncomDetails extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return ItemDetails(itemDetailModel: items[index]);
-      },
+    return Column(
+      children: items.map((e) => ItemDetails(itemDetailModel: e)).toList(),
     );
   }
 }
