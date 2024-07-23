285f689c7dc3408827d92535e6e108a7ae338b27
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-07-23 18:16:31 +0300

    Tablet layout

--- lib/widgets/income_details.dart
+++ lib/widgets/income_details.dart
@@ -29,6 +29,7 @@ class IncomDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      shrinkWrap: true,
       itemCount: items.length,
       itemBuilder: (context, index) {
         return ItemDetails(itemDetailModel: items[index]);
