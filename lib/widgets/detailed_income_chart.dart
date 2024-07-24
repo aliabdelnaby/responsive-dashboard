be3f847849287aa78d48072b065cd252c7dce9de
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-07-24 14:58:29 +0300

    Detailed income chart [2]

--- lib/widgets/detailed_income_chart.dart
+++ lib/widgets/detailed_income_chart.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_admin_dashboard/core/utils/app_styles.dart';
 
 class DetailedIncomeChart extends StatefulWidget {
   const DetailedIncomeChart({super.key});
@@ -38,28 +39,40 @@ class _IncomeChartState extends State<DetailedIncomeChart> {
       ),
       sections: [
         PieChartSectionData(
-          radius: touchedIndex == 0 ? 60 : 50,
-          showTitle: false,
           value: 40,
+          titlePositionPercentageOffset: touchedIndex == 0 ? 1.5 : null,
+          title: touchedIndex == 0 ? 'Design service' : '40%',
+          radius: touchedIndex == 0 ? 60 : 50,
           color: const Color(0xff208BC7),
+          titleStyle: AppStyles.styleMedium16
+              .copyWith(color: touchedIndex == 0 ? null : Colors.white),
         ),
         PieChartSectionData(
-          radius: touchedIndex == 1 ? 60 : 50,
-          showTitle: false,
           value: 25,
+          titlePositionPercentageOffset: touchedIndex == 1 ? 2.2 : null,
+          title: touchedIndex == 1 ? 'Design product' : '25%',
+          radius: touchedIndex == 1 ? 60 : 50,
           color: const Color(0xff4DB7F2),
+          titleStyle: AppStyles.styleMedium16
+              .copyWith(color: touchedIndex == 1 ? null : Colors.white),
         ),
         PieChartSectionData(
-          radius: touchedIndex == 2 ? 60 : 50,
-          showTitle: false,
           value: 20,
+          titlePositionPercentageOffset: touchedIndex == 2 ? 1.4 : null,
+          title: touchedIndex == 2 ? 'Product royalty' : '20%',
+          radius: touchedIndex == 2 ? 60 : 50,
           color: const Color(0xff064060),
+          titleStyle: AppStyles.styleMedium16
+              .copyWith(color: touchedIndex == 2 ? null : Colors.white),
         ),
         PieChartSectionData(
-          radius: touchedIndex == 3 ? 60 : 50,
-          showTitle: false,
           value: 22,
+          titlePositionPercentageOffset: touchedIndex == 3 ? 1.5 : null,
+          title: touchedIndex == 3 ? 'Other' : '22%',
+          radius: touchedIndex == 3 ? 60 : 50,
           color: const Color(0xFFE2DECD),
+          titleStyle: AppStyles.styleMedium16
+              .copyWith(color: touchedIndex == 3 ? null : Colors.white),
         ),
       ],
     );
